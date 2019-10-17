0a6e029a6e315a7b57677666db2c43c717d3c154
finaclemence15 <finaclemence15@gmail.com>
2019-10-17 19:08:30 +0200

    install pip install django-tinymce

--- instagramproject/settings.py
+++ instagramproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [    
     'instagramapp.apps.InstagramappConfig',
+    'tinymce',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
