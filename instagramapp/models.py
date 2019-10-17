f4f04e2d7ca2f4254362a06ab3581d7fac79b6b3
finaclemence15 <finaclemence15@gmail.com>
2019-10-17 12:43:59 +0200

    creating Image models

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+        
+class Profile(models.Model):
+    profile = models.ImageField(upload_to = 'images/')
+    bio = models.CharField(max_length =60)
+    
+    def __str__(self):
+        return self.bio
+    
+class Image (models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length =40)
+    caption= models.CharField(max_length =60)
+    likes = models.IntegerField(default='none')
+    comments= models.CharField(max_length =100)
+    profile = models.ForeignKey(Profile,null = True)
+
+    def __str__(self):
+        return self.name
+    
+class Meta:
+        ordering = ['name']        
+        
