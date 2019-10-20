0d5b0e7b401708de877ab661c4c9aa3b19cc360b
finaclemence15 <finaclemence15@gmail.com>
2019-10-20 23:57:09 +0200

    testing instance for Image model

--- instagramapp/tests.py
+++ instagramapp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from . models import Image, Profile, Comments
 
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+            # Set up method
+    def setUp(self):
+        self.butterfly= Image(image = 'book.jpg', name ='butterfly', caption ='about butterfly')
+        
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.butterfly,Image))        
